9f9bd442392846125d293f9860c376fe9749b3c7
vishnu303 <vishnu.mvk32@gmail.com>
2023-05-27 20:32:19 +0530

    Hotel ui

--- hotel_ui/lib/hotel_screen.dart
+++ hotel_ui/lib/hotel_screen.dart
@@ -8,6 +8,7 @@ class HotelScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Stack(
             children: [
@@ -96,8 +97,33 @@ class HotelScreen extends StatelessWidget {
                   ],
                 ),
                 const Spacer(),
-                TextButton(onPressed: () {}, child: Text('hhsdjd'))
+                TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                    onPressed: () {},
+                    child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 8),
+                        child: const Text(
+                          'Book Now',
+                          style: TextStyle(color: Colors.white),
+                        )))
               ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: Text(
+              'Ramadan Plaza Palm Grove',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              'Celebrate business success at the Crowne Plaza Kochi with a touch of leisure and panoramic views of backwaters and exquisite cuisine combined with rejuvenation at our Aira Spa.Crowne Plaza Kochi is ideally located on the new business district of the city NH 47 Bypass and provides easy access to Info Park Kakkanad, Cochin Special Economic Zone, M.G. Road, Cochin Port, Shipyard, Naval Base, major sightseeing areas like Fort Kochi, Mattancherry and is 45 minutes away from Cochin International Airport.The hotel offers 269 spacious business rooms and suites with excellent views of the backwaters and the city. Our variety of authentic culinary outlets, extensive spa and leisure facilities, and high-tech meeting spaces that can accommodate large gatherings, all within a tranquil waterfront setting, makes Crowne Plaza Kochi the preferred international brand for business, leisure and events.',
             ),
           )
         ],
